02ab352f3830352923c9da15913bad651b479c6a
user <me@mailaddress.com>
2024-01-19 16:09:35 +0100

    added an assert_eq in fibonnaci, and fixed virtual workspace since some projects were not included

--- fibonnaci/src/main.rs
+++ fibonnaci/src/main.rs
@@ -1,8 +1,9 @@
 fn fib(n: u32) -> u32 {
-    //todo 
+    todo!()
 }
 
 fn main() {
-    let n = 20;
-    println!("fib(n) = {}", fib(n));
+    let exemple_number : u32 = 20;
+    println!("fib(n) = {}", fib(exemple_number));
+    assert_eq!(fib(exemple_number),10946);
 }
