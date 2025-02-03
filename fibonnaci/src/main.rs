1dbadf6ca56a5039a90dc0ca80bcb5be115af7a6
user <me@mailaddress.com>
2025-02-03 15:18:52 +0100

    fixing assert equal of fib, was for 21 not 20

--- fibonnaci/src/main.rs
+++ fibonnaci/src/main.rs
@@ -3,7 +3,7 @@ fn fib(n: u32) -> u32 {
 }
 
 fn main() {
-    let exemple_number : u32 = 20;
+    let exemple_number : u32 = 21;
     println!("fib(n) = {}", fib(exemple_number));
     assert_eq!(fib(exemple_number),10946);
 }
